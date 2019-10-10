732233fbdd30d773f782b2a4df6b77ed9f8b4981
Roman Nemchinov <nemchinovrp@yandex.ru>
2019-10-10 09:41:48 +0300

    added spring admin client

--- src/main/java/com/nemchinovrp/serverproductmanagement/config/WebSecurityConfig.java
+++ src/main/java/com/nemchinovrp/serverproductmanagement/config/WebSecurityConfig.java
@@ -35,7 +35,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and()
                 .authorizeRequests()
                 //These are public paths
-                .antMatchers("/resources/**", "/error", "/api/user/**").permitAll()
+                .antMatchers("/resources/**", "/error", "/api/user/**", "/management/**").permitAll()
                 //These can be reachable for just have admin role.
                 .antMatchers("/api/admin/**").hasRole("ADMIN")
                 //All remaining paths should need authentication.
